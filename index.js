9d009f3e0c308eabdc37859a2483b37bafcd4643
Bartowski07 <126920086+Bartowski07@users.noreply.github.com>
2023-05-12 16:41:12 +0200

    error fix

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.POPRT || 8080;
+const cors = require("cors");
 
 // Importing all Routes
 const artistsRouter = require("./routes/artists");
+
+app.use(cors());
+
+app.get("/artists", (req, res) => {
+  // send the response with the Access-Control-Allow-Origin header
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send("list of artists");
+});
 
 // List of routes - initial endpoints
 app.use("/", artistsRouter);
